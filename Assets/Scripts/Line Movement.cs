f48b7ab2b160daeea7406f3611958d0e6cd08a56
tmicha9528 <64509091+tmicha9528@users.noreply.github.com>
2023-01-02 21:06:08 -0500

    Current Changes

---
+++ Assets/Scripts/Line Movement.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+using TMPro;
+
+public class LineMovement : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject ball_line;
+    float ballXPosition = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball.transform.position = new Vector3(0, 0, 0);
+        ball_line.transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
